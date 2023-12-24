d8911a73add958a3a59e8b9c17c61a9c8df44631
jyron <jyron.dev@gmail.com>
2023-12-23 20:31:53 -0600

    Props naming

--- stockranker_frontend/src/components/disqusComments.jsx
+++ stockranker_frontend/src/components/disqusComments.jsx
@@ -2,7 +2,7 @@ import { DiscussionEmbed } from "disqus-react"
 const DisqusComments = ({ post }) => {
     const disqusShortname = "stockranker"
     const disqusConfig = {
-        url: `https://stockranker.co/`,
+        url: `https://stockranker.co/stocks/${post.ticker}`,
         identifier: post.id, // Single post id
 
     }
