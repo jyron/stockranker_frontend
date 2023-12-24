2b1360a37b69064736a1ae23514a4a299a077ba6
jyron <jyron.dev@gmail.com>
2023-12-24 02:52:20 -0600

    Remove runtime config

--- stockranker_frontend/vite.config.js
+++ stockranker_frontend/vite.config.js
@@ -5,10 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/',
   plugins: [react()],
-  resolve: {
-    alias: {
-      "./runtimeConfig": "./runtimeConfig.browser",
-    },
-},
 })
 
