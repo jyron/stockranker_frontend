3cc60306d94e7701974f13a65e355c810bdf4825
jyron <jyron.dev@gmail.com>
2023-12-24 16:31:48 -0600

    gradient and comments

--- stockranker_frontend/src/pages/Home.jsx
+++ stockranker_frontend/src/pages/Home.jsx
@@ -6,14 +6,22 @@ import StockCarousel from '../components/StockCarousel';
 import Table from '../components/table';
 import Footer from '../components/footer';
 
+
+
 const Home = () => {
+
+    const gradientBackgroundStyle = {
+        background: 'linear-gradient(to right, #ff7e5f, #feb47b)', // Change the colors as needed
+        height: '100vh', // Adjust the height to cover the entire viewport
+        overflow: 'hidden', // Ensure the gradient covers the entire viewport
+    };
     return (
-        <>
+        <div className="bg-gradient-to-r from-red-500 to-yellow-500 min-h-screen overflow-hidden">
             <Header />
             <StockCarousel />
             <Table />
             <Footer />
-        </>
+        </div>
     );
 };
 
