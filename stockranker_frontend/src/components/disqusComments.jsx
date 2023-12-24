853a8d01fd64f5bd99a7d054eac7905ad8ff9bdb
jyron <jyron.dev@gmail.com>
2023-12-24 14:59:58 -0600

    disqus errors

--- stockranker_frontend/src/components/disqusComments.jsx
+++ stockranker_frontend/src/components/disqusComments.jsx
@@ -1,33 +1,18 @@
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import { DiscussionEmbed } from 'disqus-react';
-
+import { DiscussionEmbed } from "disqus-react"
 const DisqusComments = ({ post }) => {
-    const disqusShortname = 'stockranker';
-    const location = useLocation();
+    const disqusShortname = "stockranker"
+    const disqusConfig = {
+        url: `https://stockranker.co/stocks/${post.ticker}`,
+        identifier: post.ticker + "_0", // Single post id
 
-    useEffect(() => {
-        // Reload Disqus comments whenever the route changes
-        const loadDisqusComments = () => {
-            const disqusConfig = {
-                url: `https://stockranker.co${location.pathname}`,
-                identifier: post.id + '_0', // Single post id
-            };
-
-            // Assuming you have a function to reset Disqus comments
-            window.DISQUS.reset({
-                reload: true,
-                config: function () {
-                    this.page.identifier = disqusConfig.identifier;
-                    this.page.url = disqusConfig.url;
-                },
-            });
-        };
-
-        loadDisqusComments();
-    }, [location.pathname, post.id]);
-
-    return <DiscussionEmbed shortname={disqusShortname} config={{}} />;
-};
-
+    }
+    return (
+        <>
+            <DiscussionEmbed
+                shortname={disqusShortname}
+                config={disqusConfig}
+            />
+        </>
+    )
+}
 export default DisqusComments;
