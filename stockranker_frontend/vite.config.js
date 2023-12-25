23d68750a3df6493393c59e9ff6c08c44b7ed6d5
jyron <jyron.dev@gmail.com>
2023-12-24 18:54:44 -0600

    FIXED REFRESH BY CHANGING VITE CONFIG

--- stockranker_frontend/vite.config.js
+++ stockranker_frontend/vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/', // Adjust the base to the subdirectory path
   plugins: [react()],
-})
-
+});
